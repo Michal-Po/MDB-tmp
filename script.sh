fb03ec9d61be57afe94d9953ec0c57f18a9b1999
Michal Podrouzek <michal.podrouzek@cloudfield.cz>
2019-11-25 20:46:58 +0100

    edit config file

--- script.sh
+++ script.sh
@@ -9,9 +9,9 @@ printf '\nN\nY\nY\nY\nY\n' | sudo mysql_secure_installation
 # sudo mysql < batch-file.sql
 
 # MariaDB configuration
-# sudo sed -i 's/bind-address*/# bind-address/' /etc/mysql/mariadb.conf.d/50-server.cnf
-# sudo sed -i 's/#server-id*/server-id/' /etc/mysql/mariadb.conf.d/50-server.cnf
+sudo sed -i 's/bind-address/# bind-address/' /etc/mysql/mariadb.conf.d/50-server.cnf
+sudo sed -i 's/#server-id*/server-id/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 # Mariadb restart
-# sudo systemctl restart mariadb
+sudo systemctl restart mariadb
 
